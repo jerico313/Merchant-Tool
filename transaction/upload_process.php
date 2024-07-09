c27b821f14fc562c34fcc8e16e0defb1da8b1275
Jerico <jericobuncag0@gmail.com>
2024-07-09 10:39:56 +0800

    updated upload_process.php

--- transaction/upload_process.php
+++ transaction/upload_process.php
@@ -4,13 +4,63 @@
     <link rel="stylesheet" href="../style.css">
     <title>Upload Success</title>
     <style>
-      body {
-      background-image: url("../images/bg_booky.png");
-      background-position: center;
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-attachment: fixed;
-    }
+        body {
+            background-image: url("../images/bg_booky.png");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+        }
+
+        .container {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+            border: solid #fff 2px;
+            border-radius:10px;
+            width:250px;
+            height:350px;
+            padding-top:70px;
+            backdrop-filter: blur(16px) saturate(180%);
+            -webkit-backdrop-filter: blur(16px) saturate(180%);
+            background-color: rgba(255, 255, 255, 0.40);
+            border-radius: 12px;
+            border: 1px solid rgba(209, 213, 219, 0.3);
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+        }
+        .checkmark {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+            display: block;
+            margin: 0 auto;
+        }
+        .checkmark__circle {
+            stroke-width: 4;
+            stroke-miterlimit: 10;
+            stroke: #4caf50;
+            fill: none;
+        }
+        .checkmark__check {
+            stroke-dasharray: 48;
+            stroke-dashoffset: 48;
+            stroke-width: 4;
+            stroke-linecap: round;
+            stroke-miterlimit: 10;
+            stroke: #4caf50;
+            fill: none;
+            animation: draw 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+        }
+        @keyframes draw {
+            0% {
+                stroke-dashoffset: 48;
+            }
+            100% {
+                stroke-dashoffset: 0;
+            }
+        }
     </style>
 </head>
 <?php
