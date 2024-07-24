8aa48d6fda7142d6f30e02c97f220a307ff19ba6
Jerico <jericobuncag0@gmail.com>
2024-07-24 16:01:44 +0800

    #

--- fee/edit.php
+++ fee/edit.php
@@ -3,18 +3,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include("../inc/config.php");
 
     $feeId = $_POST['feeId'];
-    $paymayaCreditCard = $_POST['paymayaCreditCard'];
-    $gcash = $_POST['gcash'];
-    $gcashMiniapp = $_POST['gcashMiniapp'];
-    $paymaya = $_POST['paymaya'];
-    $leadgenCommission = $_POST['leadgenCommission'];
+    
+    $paymayaCreditCard = substr($_POST['paymayaCreditCard'], 0, -1);
+    $gcash = substr($_POST['gcash'], 0, -1);
+    $gcashMiniapp = substr($_POST['gcashMiniapp'], 0, -1);
+    $paymaya = substr($_POST['paymaya'], 0, -1);
+    $gcash = substr($_POST['gcash'], 0, -1);
+    $gcashMiniapp = substr($_POST['gcashMiniapp'], 0, -1);
+    $paymaya = substr($_POST['paymaya'], 0, -1);
+    $leadgenCommission = substr($_POST['leadgenCommission'], 0, -1);
     $commissionType = $_POST['commissionType'];
-    $cwtRate = $_POST['cwtRate'];
+    $cwtRate = substr($_POST['cwtRate'], 0, -1);
     $userId = $_POST['userId'];
 
     // Update fee details
     $stmt = $conn->prepare("UPDATE fee SET paymaya_credit_card=?, gcash=?, gcash_miniapp=?, paymaya=?, maya_checkout=?, maya=?, lead_gen_commission=?, commission_type=?, cwt_rate=? WHERE fee_id=?");
-    $stmt->bind_param("sssssssss", $paymayaCreditCard, $gcash, $gcashMiniapp, $paymaya,  $paymayaCreditCard,  $paymayaCreditCard, $leadgenCommission, $commissionType, $cwtRate, $feeId);
+    $stmt->bind_param("ssssssssss", $paymayaCreditCard, $gcash, $gcashMiniapp, $paymaya,  $paymayaCreditCard,  $paymayaCreditCard, $leadgenCommission, $commissionType, $cwtRate, $feeId);
 
     if ($stmt->execute()) {
         $stmt = $conn->prepare("SELECT activity_id FROM activity_history ORDER BY created_at DESC LIMIT 1");
