2cfd53288e433ba88990a75276278eea3ed245cf
Gwen <agathagwen06@gmail.com>
2024-08-01 09:14:51 +0800

    deleted upload.php and upload_process in merchant/store folder

--- store/upload_process.php
+++ store/upload_process.php
@@ -209,12 +209,12 @@ if (isset($_FILES['fileToUpload']['name']) && $_FILES['fileToUpload']['name'] !=
     // If no duplicates and all merchant IDs are valid, proceed with inserting into database
     $handle = fopen($file_tmp, "r");
     fgetcsv($handle); // Skip header row again
-    $stmt = $conn->prepare("INSERT INTO store (merchant_id, store_id, store_name, legal_entity_name, store_address, email_address) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO store (store_id, merchant_id, store_name, legal_entity_name, store_address, email_address) VALUES (?, ?, ?, ?, ?, ?)");
     while (($data = fgetcsv($handle)) !== FALSE) {
         $data[4] = empty($data[4]) ? null : $data[4]; // Convert blank legal_entity_name to null
         $data[5] = empty($data[5]) ? null : $data[5]; // Convert blank store_address to null
         $data[6] = empty($data[6]) ? null : $data[6]; // Convert blank email_address to null
-        $stmt->bind_param("ssssss", $data[1], $data[3], $data[2], $data[4], $data[5], $data[6]); // Adjust based on your CSV structure
+        $stmt->bind_param("ssssss", $data[3], $data[1], $data[2], $data[4], $data[5], $data[6]); // Adjust based on your CSV structure
         $stmt->execute();
     }
 
