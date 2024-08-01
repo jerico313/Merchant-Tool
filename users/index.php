96d30024a0c2e80cffd694c17548dc999a96f4ac
Jerico <jericobuncag0@gmail.com>
2024-08-01 12:46:15 +0800

    updated merchant/index.php and users/index.php

--- users/index.php
+++ users/index.php
@@ -115,10 +115,154 @@ function displayUser()
       outline: none;
       box-shadow: none;
     }
+
+    .loading {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 80vh;
+      font-size: 18px;
+      color: #333;
+      font-weight: 800;
+    }
+
+    .cont-box {
+      display: none;
+    }
+
+
+    .lds-default,
+    .lds-default div {
+      box-sizing: border-box;
+    }
+
+    .lds-default {
+      display: inline-block;
+      position: relative;
+      width: 80px;
+      height: 80px;
+      color: #E96529;
+    }
+
+    .lds-default div {
+      position: absolute;
+      width: 6.4px;
+      height: 6.4px;
+      background: currentColor;
+      border-radius: 50%;
+      animation: lds-default 1.2s linear infinite;
+    }
+
+    .lds-default div:nth-child(1) {
+      animation-delay: 0s;
+      top: 36.8px;
+      left: 66.24px;
+    }
+
+    .lds-default div:nth-child(2) {
+      animation-delay: -0.1s;
+      top: 22.08px;
+      left: 62.29579px;
+    }
+
+    .lds-default div:nth-child(3) {
+      animation-delay: -0.2s;
+      top: 11.30421px;
+      left: 51.52px;
+    }
+
+    .lds-default div:nth-child(4) {
+      animation-delay: -0.3s;
+      top: 7.36px;
+      left: 36.8px;
+    }
+
+    .lds-default div:nth-child(5) {
+      animation-delay: -0.4s;
+      top: 11.30421px;
+      left: 22.08px;
+    }
+
+    .lds-default div:nth-child(6) {
+      animation-delay: -0.5s;
+      top: 22.08px;
+      left: 11.30421px;
+    }
+
+    .lds-default div:nth-child(7) {
+      animation-delay: -0.6s;
+      top: 36.8px;
+      left: 7.36px;
+    }
+
+    .lds-default div:nth-child(8) {
+      animation-delay: -0.7s;
+      top: 51.52px;
+      left: 11.30421px;
+    }
+
+    .lds-default div:nth-child(9) {
+      animation-delay: -0.8s;
+      top: 62.29579px;
+      left: 22.08px;
+    }
+
+    .lds-default div:nth-child(10) {
+      animation-delay: -0.9s;
+      top: 66.24px;
+      left: 36.8px;
+    }
+
+    .lds-default div:nth-child(11) {
+      animation-delay: -1s;
+      top: 62.29579px;
+      left: 51.52px;
+    }
+
+    .lds-default div:nth-child(12) {
+      animation-delay: -1.1s;
+      top: 51.52px;
+      left: 62.29579px;
+    }
+
+    @keyframes lds-default {
+
+      0%,
+      20%,
+      80%,
+      100% {
+        transform: scale(1);
+      }
+
+      50% {
+        transform: scale(1.5);
+      }
+    }
+  </style>
   </style>
 </head>
 
 <body>
+<div class="loading">
+    <div>
+      <div class="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    Loading, Please wait...
+  </div>
   <div class="cont-box">
     <div class="custom-box pt-4">
       <div class="sub" style="text-align:left;">
@@ -298,23 +442,20 @@ function displayUser()
 
 <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
-    <script>
-      $(document).ready(function () {
-        if ($.fn.DataTable.isDataTable('#example')) {
-          $('#example').DataTable().destroy();
-        }
+        <script>
+      $(window).on('load', function () {
+        $('.loading').hide();
+        $('.cont-box').show();
 
-        $('#example').DataTable({
+        var table = $('#example').DataTable({
           scrollX: true,
           columnDefs: [
             { orderable: false, targets: [0] }
           ],
-          order: []  // Ensure no initial ordering
-        });
-      });
-
+      order: []
+   }); 
+  });
     </script>
-
     <!-- Edit Merchant Modal -->
     <script>
       function editUser(userUuid) {
