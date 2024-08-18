beb8efa3872dd051c3ba007eb0bf2653859fbc5f
Gwen <agathagwen06@gmail.com>
2024-08-18 21:19:43 +0800

    fixed merge conflicts

--- merchant/store/decoupled_generate_report.php
+++ merchant/store/decoupled_generate_report.php
@@ -30,20 +30,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         $stmt->close(); // Close the first statement
 
-<<<<<<< HEAD
-        // Get the latest activity_id from activity_history
-        $latestActivityId = null;
-        $stmt = $conn->prepare("SELECT activity_id FROM activity_history ORDER BY created_at DESC LIMIT 1");
-        if ($stmt) {
-            $stmt->execute();
-            $stmt->bind_result($latestActivityId);
-            $stmt->fetch(); // Fetch the result
-            $stmt->close(); // Close the statement
-        }
-
-        // Get the max coupled_report_id from report_history_coupled
-=======
->>>>>>> f45f0401895939a64df1b5cd733cad324b9ab35b
         $maxDecoupledReportId = null;
         $stmt = $conn->prepare("SELECT decoupled_report_id FROM report_history_decoupled ORDER BY created_at DESC LIMIT 1");
         if ($stmt) {
@@ -53,11 +39,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->close(); // Close the statement
         }
 
-<<<<<<< HEAD
-        if ($latestActivityId !== null && $maxDecoupledReportId !== null) {
-            // Update activity_history with user_id
-            $stmt = $conn->prepare("UPDATE activity_history SET user_id=? WHERE activity_id=?");
-=======
         if ($maxDecoupledReportId !== null) {
             $stmt1 = $conn->prepare("UPDATE report_history_decoupled SET generated_by=? WHERE decoupled_report_id=?");
             if ($stmt1) {
@@ -67,7 +48,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
 
             $stmt = $conn->prepare("UPDATE activity_history SET user_id=? WHERE table_id=?");
->>>>>>> f45f0401895939a64df1b5cd733cad324b9ab35b
             if ($stmt) {
                 $stmt->bind_param("ss", $userId, $maxDecoupledReportId);
                 $stmt->execute();
