98aa8e98499afa8c5b8360b1622940a889fb6181
Gwen <agathagwen06@gmail.com>
2024-09-20 19:03:44 +0800

    fixed column index of transaction_id in transactions/upload_process (data[7] to data[9])

--- transactions/upload_process.php
+++ transactions/upload_process.php
@@ -241,7 +241,7 @@ if (isset($_FILES['fileToUpload']['name']) && $_FILES['fileToUpload']['name'] !=
         $data[14] = str_replace(',', '', $data[14]);
         $data[15] = ($data[15] = str_replace('"', '', $data[15])) === '' ? null : $data[15]; 
 
-        $stmt1->bind_param("sssssssssssssss", $data[7], $data[3], $data[6], $data[7], $data[8], $data[5], $data[4], $transaction_date, $data[11], $data[12], $data[13], $data[14], $data[15], $data[16], $data[17]);
+        $stmt1->bind_param("sssssssssssssss", $data[9], $data[3], $data[6], $data[7], $data[8], $data[5], $data[4], $transaction_date, $data[11], $data[12], $data[13], $data[14], $data[15], $data[16], $data[17]);
         $stmt1->execute();
         
         updateActivityHistory($conn, $data[5], $userId);
